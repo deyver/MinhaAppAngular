3d6f1f3984bed0f6b318389559ccf1b844de80c4
Gomes <deyver.gomes@devpartner.com.br>
2022-10-30 22:33:30 -0300

    Componentes

---
+++ src/app/nome-componente/nome-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  templateUrl: './nome-componente.component.html'
+})
+export class NcmeComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
